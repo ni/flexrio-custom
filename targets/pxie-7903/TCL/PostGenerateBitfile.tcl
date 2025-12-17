67b14f89dbd8d0fb461af48ee43c76bc57417077
Salvador Santoluctio <salvador.santolucito@ni.com>
2025-12-17 15:22:39 -0600

    refactor for flexrio deps

--- targets/pxie-7903/TCL/PostGenerateBitfile.tcl
+++
@@ -1,8 +0,0 @@
-#githubvisible=true
-
-# Clear PYTHONPATH and PYTHONHOME
-set env(PYTHONPATH) ""
-set env(PYTHONHOME) ""
-
-# Execute the Python script
-exec nihdl create-lvbitx
--- targets/pxie-7903/TCL/PreSynthesize.tcl
+++
@@ -1,2 +0,0 @@
-#githubvisible=true
-set_param synth.elaboration.rodinMoreOptions "rt::set_parameter max_loop_limit 1000000;"

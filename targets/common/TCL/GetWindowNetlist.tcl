18eefb25991a99f0e8ccba83ea225f47b9469f9b
Salvador Santoluctio <salvador.santolucito@ni.com>
2026-03-13 17:23:16 -0500

    adjusting to new base target

--- targets/common/TCL/GetWindowNetlist.tcl
+++ targets/common/TCL/GetWindowNetlist.tcl
@@ -1,6 +1,6 @@
 # githubvisible=true
 
-set_property top TheWindowFlatWrapper [current_fileset]
+set_property top TheLvWindowFlatWrapper [current_fileset]
 set_property -name {STEPS.SYNTH_DESIGN.ARGS.MORE OPTIONS} -value {-mode out_of_context} -objects [get_runs synth_1]
 set_msg_config -id {*INFO*} -suppress
 set_msg_config -id {*WARNING*} -suppress
@@ -10,4 +10,4 @@ reset_run synth_1
 launch_runs synth_1 -jobs 11
 wait_on_run synth_1
 open_run synth_1 -name synth_1
-write_verilog TheWindowFlatWrapper.v
+write_verilog TheLvWindowFlatWrapper.v
